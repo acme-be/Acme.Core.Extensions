f4dc7e43d27d0db227f80cdb16e8507e00780329
Simon Baudart <s.baudart@outlook.com>
2018-02-02 16:44:17 +0100

    • Add the ObjectExtensions

---
+++ Acme.Core.Extensions.Tests/ObjectExtensionsTest.cs
@@ -0,0 +1,114 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ObjectExtensionsTest.cs" company="Acme">
+//  Copyright (c) Acme. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Acme.Core.Extensions.Tests
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    ///     The object extensions test.
+    /// </summary>
+    [TestClass]
+    public class ObjectExtensionsTest
+    {
+        /// <summary>
+        ///     Ensure not null with class.
+        /// </summary>
+        [TestMethod]
+        public void EnsureNotNullWithClass()
+        {
+            var t = new object();
+            var u = t.EnsureNotNull(nameof(t));
+            Assert.AreEqual(t, u);
+        }
+
+        /// <summary>
+        ///     Ensure not null with class and null value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnsureNotNullWithNullClass()
+        {
+            object t = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            t.EnsureNotNull(nameof(t));
+        }
+
+        /// <summary>
+        ///     Ensure not null with struct and null value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EnsureNotNullWithNullStruct()
+        {
+            int? t = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            t.EnsureNotNull(nameof(t));
+        }
+
+        /// <summary>
+        ///     Ensure not null with struct.
+        /// </summary>
+        [TestMethod]
+        public void EnsureNotNullWithStruct()
+        {
+            int? t = 42;
+            var u = t.EnsureNotNull(nameof(t));
+            Assert.AreEqual(t, u);
+        }
+
+        /// <summary>
+        ///     Throw if null with class.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfNullWithClass()
+        {
+            var t = new object();
+            t.ThrowIfNull(nameof(t));
+        }
+
+        /// <summary>
+        ///     Throw if null with class and null value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowIfNullWithNullClass()
+        {
+            object t = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            t.ThrowIfNull(nameof(t));
+        }
+
+        /// <summary>
+        ///     Throw if null with struct and null value.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowIfNullWithNullStruct()
+        {
+            int? t = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            t.ThrowIfNull(nameof(t));
+        }
+
+        /// <summary>
+        ///     Throw if null with struct.
+        /// </summary>
+        [TestMethod]
+        public void ThrowIfNullWithStruct()
+        {
+            int? t = 42;
+            t.ThrowIfNull(nameof(t));
+        }
+    }
+}
---
+++ Acme.Core.Extensions/ObjectExtensions.cs
@@ -0,0 +1,81 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ObjectExtensions.cs" company="Acme">
+//  Copyright (c) Acme. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Acme.Core.Extensions
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    ///     The object extensions.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        ///     Ensures the specified <paramref name="value" /> is not null. Otherwise it throws an
+        ///     <see cref="ArgumentNullException" />.
+        /// </summary>
+        /// <typeparam name="T">The object type</typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="parameterName">Name of the parameter.</param>
+        /// <returns>the specified <paramref name="value" />.</returns>
+        public static T EnsureNotNull<T>(this T value, string parameterName)
+            where T : class
+        {
+            value.ThrowIfNull(parameterName);
+            return value;
+        }
+
+        /// <summary>
+        ///     Ensures the specified <paramref name="value" /> is not null. Otherwise it throws an
+        ///     <see cref="ArgumentNullException" />.
+        /// </summary>
+        /// <typeparam name="T">The object type</typeparam>
+        /// <param name="value">The value.</param>
+        /// <param name="parameterName">Name of the parameter.</param>
+        /// <returns>the specified <paramref name="value" />.</returns>
+        public static T? EnsureNotNull<T>(this T? value, string parameterName)
+            where T : struct
+        {
+            value.ThrowIfNull(parameterName);
+            return value;
+        }
+
+        /// <summary>
+        ///     Extension method on object, which throws an <see cref="ArgumentNullException" /> if
+        ///     <paramref name="value" /> is null.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="value">The value to check.</param>
+        /// <param name="parameterName">Name of the parameter.</param>
+        /// <exception cref="ArgumentNullException">Value cannot be null. Parameter name: <paramref name="parameterName" />.</exception>
+        public static void ThrowIfNull<T>(this T value, string parameterName)
+            where T : class
+        {
+            if (ReferenceEquals(value, null))
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        /// <summary>
+        ///     Extension method on object, which throws an <see cref="ArgumentNullException" /> if
+        ///     <paramref name="value" /> is null.
+        /// </summary>
+        /// <typeparam name="T">The object type.</typeparam>
+        /// <param name="value">The value to check.</param>
+        /// <param name="parameterName">Name of the parameter.</param>
+        /// <exception cref="ArgumentNullException">Value cannot be null. Parameter name: <paramref name="parameterName" />.</exception>
+        public static void ThrowIfNull<T>(this T? value, string parameterName)
+            where T : struct
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+    }
+}
