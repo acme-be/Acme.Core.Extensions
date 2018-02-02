9b601c64d5fc39445edee4c4531575d1c678cfe4
Simon Baudart <s.baudart@outlook.com>
2018-02-02 16:37:00 +0100

    • add the DateTimeExtensions

---
+++ Acme.Core.Extensions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="DateTimeExtensionsTests.cs" company="Acme">
+//  Copyright (c) Acme. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Acme.Core.Extensions.Tests
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    ///     The date time extensions tests.
+    /// </summary>
+    [TestClass]
+    public class DateTimeExtensionsTests
+    {
+        /// <summary>
+        /// To the unix time stamp.
+        /// </summary>
+        [TestMethod]
+        public void ToUnixTimeStamp()
+        {
+            var date = new DateTime(1983, 05, 14, 07, 30, 42);
+            var unixTimeStamp = date.ToUnixTimeStamp();
+            Assert.AreEqual(421738242, unixTimeStamp);
+        }
+    }
+}
---
+++ Acme.Core.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="DateTimeExtensions.cs" company="Acme">
+//  Copyright (c) Acme. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Acme.Core.Extensions
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// All the datetime extensions
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Convert DateTime to the unix time stamp.
+        /// </summary>
+        /// <param name="date">The date.</param>
+        /// <returns>The timestamp in unix format</returns>
+        public static int ToUnixTimeStamp(this DateTime date)
+        {
+            return (int)date.ToUniversalTime().Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+    }
+}
