446bc687f8fe2251dc0025dcae56190fbc24a3f9
BAUDART Simon <bdrsmn@forem.be>
2022-01-20 15:03:18 +0100

    test: Switch tests to xUnit

--- Acme.Core.Extensions.Tests/ByteArrayExtensionsTests.cs
+++ Acme.Core.Extensions.Tests/ByteArrayExtensionsTests.cs
@@ -4,39 +4,34 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-namespace Acme.Core.Extensions.Tests
-{
-    using System;
-    using System.Linq;
+namespace Acme.Core.Extensions.Tests;
 
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+/// <summary>
+/// The byte array extensions tests.
+/// </summary>
+public class ByteArrayExtensionsTests
+{
+    /// <summary>
+    /// To the hexadecimal string.
+    /// </summary>
+    [Fact]
+    public void ToHexadecimalString()
+    {
+        var array = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var hexadecimalString = array.ToHexadecimalString();
+        Assert.Equal("010203040506070809", hexadecimalString);
+    }
 
     /// <summary>
-    /// The byte array extensions tests.
+    /// To the hexadecimal string with empty
     /// </summary>
-    [TestClass]
-    public class ByteArrayExtensionsTests
+    [Fact]
+    public void ToHexadecimalStringEmpty()
     {
-        /// <summary>
-        /// To the hexadecimal string.
-        /// </summary>
-        [TestMethod]
-        public void ToHexadecimalString()
-        {
-            var array = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var hexadecimalString = array.ToHexadecimalString();
-            Assert.AreEqual("010203040506070809", hexadecimalString);
-        }
-
-        /// <summary>
-        /// To the hexadecimal string with empty
-        /// </summary>
-        [TestMethod]
-        public void ToHexadecimalStringEmpty()
-        {
-            var array = new byte[0];
-            var hexadecimalString = array.ToHexadecimalString();
-            Assert.AreEqual(string.Empty, hexadecimalString);
-        }
+        var array = new byte[0];
+        var hexadecimalString = array.ToHexadecimalString();
+        Assert.Equal(string.Empty, hexadecimalString);
     }
 }
--- Acme.Core.Extensions.Tests/ObjectExtensionsTest.cs
+++ Acme.Core.Extensions.Tests/ObjectExtensionsTest.cs
@@ -4,136 +4,130 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-namespace Acme.Core.Extensions.Tests
+namespace Acme.Core.Extensions.Tests;
+
+using System;
+
+using Acme.Core.Extensions.Exceptions;
+
+using Xunit;
+
+/// <summary>
+/// The object extensions test.
+/// </summary>
+public class ObjectExtensionsTest
 {
-    using System;
-    using System.Linq;
-
-    using Acme.Core.Extensions.Exceptions;
-
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    /// <summary>
+    /// Ensure not null with class.
+    /// </summary>
+    [Fact]
+    public void EnsureNotNullWithClass()
+    {
+        var t = new object();
+        var u = t.EnsureNotNull(nameof(t));
+        Assert.Equal(t, u);
+    }
 
     /// <summary>
-    /// The object extensions test.
+    /// Ensure not null with class and null value.
     /// </summary>
-    [TestClass]
-    public class ObjectExtensionsTest
+    [Fact]
+    public void EnsureNotNullWithNullClass()
     {
-        /// <summary>
-        /// Ensure not null with class.
-        /// </summary>
-        [TestMethod]
-        public void EnsureNotNullWithClass()
-        {
-            var t = new object();
-            var u = t.EnsureNotNull(nameof(t));
-            Assert.AreEqual(t, u);
-        }
+        object? t = null;
 
-        /// <summary>
-        /// Ensure not null with class and null value.
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(ObjectIsNullException))]
-        public void EnsureNotNullWithNullClass()
-        {
-            object t = null;
+        // ReSharper disable once ExpressionIsAlwaysNull
+        Assert.Throws<ObjectIsNullException>(() => t.EnsureNotNull(nameof(t)));
 
-            // ReSharper disable once ExpressionIsAlwaysNull
-            t.EnsureNotNull(nameof(t));
-        }
+    }
 
-        /// <summary>
-        /// Ensure not null with struct and null value.
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(ObjectIsNullException))]
-        public void EnsureNotNullWithNullStruct()
-        {
-            int? t = null;
+    /// <summary>
+    /// Ensure not null with struct and null value.
+    /// </summary>
+    [Fact]
+    public void EnsureNotNullWithNullStruct()
+    {
+        int? t = null;
 
-            // ReSharper disable once ExpressionIsAlwaysNull
-            t.EnsureNotNull(nameof(t));
-        }
+        // ReSharper disable once ExpressionIsAlwaysNull
+        Assert.Throws<ObjectIsNullException>(() => t.EnsureNotNull(nameof(t)));
+    }
 
-        /// <summary>
-        /// Ensure not null with struct.
-        /// </summary>
-        [TestMethod]
-        public void EnsureNotNullWithStruct()
-        {
-            int? t = 42;
-            var u = t.EnsureNotNull(nameof(t));
-            Assert.AreEqual(t, u);
-        }
+    /// <summary>
+    /// Ensure not null with struct.
+    /// </summary>
+    [Fact]
+    public void EnsureNotNullWithStruct()
+    {
+        int? t = 42;
+        var u = t.EnsureNotNull(nameof(t));
+        Assert.Equal(t, u);
+    }
 
-        /// <summary>
-        /// Throw if null with class.
-        /// </summary>
-        [TestMethod]
-        public void ThrowIfNullWithClass()
-        {
-            var t = new object();
-            t.ThrowIfNull(nameof(t));
-        }
+    /// <summary>
+    /// Throw if null with class.
+    /// </summary>
+    [Fact]
+    public void ThrowIfNullWithClass()
+    {
+        var t = new object();
+        t.ThrowIfNull(nameof(t));
+    }
 
-        /// <summary>
-        /// Throw if null with class and null value.
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ThrowIfNullWithNullClass()
-        {
-            object t = null;
+    /// <summary>
+    /// Throw if null with class and null value.
+    /// </summary>
+    [Fact]
+    public void ThrowIfNullWithNullClass()
+    {
+        object? t = null;
 
-            // ReSharper disable once ExpressionIsAlwaysNull
-            t.ThrowIfNull(nameof(t));
-        }
+        // ReSharper disable once ExpressionIsAlwaysNull
+        Assert.Throws<ArgumentNullException>(() => t.ThrowIfNull(nameof(t)));
+    }
 
-        /// <summary>
-        /// Throw if null with struct and null value.
-        /// </summary>
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ThrowIfNullWithNullStruct()
-        {
-            int? t = null;
+    /// <summary>
+    /// Throw if null with struct and null value.
+    /// </summary>
+    [Fact]
+    public void ThrowIfNullWithNullStruct()
+    {
+        int? t = null;
 
-            // ReSharper disable once ExpressionIsAlwaysNull
-            t.ThrowIfNull(nameof(t));
-        }
+        // ReSharper disable once ExpressionIsAlwaysNull
+        Assert.Throws<ArgumentNullException>(() => t.ThrowIfNull(nameof(t)));
+    }
 
-        /// <summary>
-        /// Throw if null with struct.
-        /// </summary>
-        [TestMethod]
-        public void ThrowIfNullWithStruct()
-        {
-            int? t = 42;
-            t.ThrowIfNull(nameof(t));
-        }
+    /// <summary>
+    /// Throw if null with struct.
+    /// </summary>
+    [Fact]
+    public void ThrowIfNullWithStruct()
+    {
+        int? t = 42;
+        t.ThrowIfNull(nameof(t));
+    }
 
-        /// <summary>
-        /// Test the method xecuteOrNull with a simple cas.
-        /// </summary>
-        [TestMethod]
-        public void ExecuteOrNullSimple()
-        {
-            var value = "Have a lot of fun !";
-            var returned = value.ExecuteOrNull((x) => x.Length);
-            Assert.AreEqual(19, returned);
-        }
+    /// <summary>
+    /// Test the method xecuteOrNull with a simple cas.
+    /// </summary>
+    [Fact]
+    public void ExecuteOrNullSimple()
+    {
+        var value = "Have a lot of fun !";
+        var returned = value.ExecuteOrNull((x) => x.Length);
+        Assert.Equal(19, returned);
+    }
 
-        /// <summary>
-        /// Test the method xecuteOrNull with a null value.
-        /// </summary>
-        [TestMethod]
-        public void ExecuteOrNullWithNull()
-        {
-            string value = null;
-            // ReSharper disable once ExpressionIsAlwaysNull
-            var returned = value.ExecuteOrNull((x) => x.Length);
-            Assert.AreEqual(default, returned);
-        }
+    /// <summary>
+    /// Test the method xecuteOrNull with a null value.
+    /// </summary>
+    [Fact]
+    public void ExecuteOrNullWithNull()
+    {
+        string? value = null;
+        // ReSharper disable once ExpressionIsAlwaysNull
+        var returned = value.ExecuteOrNull((x) => x!.Length);
+        Assert.Equal(default, returned);
     }
 }
